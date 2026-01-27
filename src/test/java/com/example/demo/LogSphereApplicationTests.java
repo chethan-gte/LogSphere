2a9e7e1caad3f8b02789d31cbc4ae727fb88ea6b
kishore-gt <Window controlAboutkishore@gtechnohubb.com>
2026-01-27 14:55:54 +0530

    changed upload

--- src/test/java/com/example/demo/LogSphereApplicationTests.java
+++ src/test/java/com/example/demo/LogSphereApplicationTests.java
@@ -6,6 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LogSphereApplicationTests {
 
+	@org.springframework.boot.test.mock.mockito.MockBean
+	private com.example.demo.service.EmailService emailService;
+
 	@Test
 	void contextLoads() {
 	}
